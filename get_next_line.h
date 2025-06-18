22cd888cb0ee742f851d07bbf48131db7897a6f1
Angelo Rios <angelorios@live.com>
2025-06-18 22:18:59 +0200

    add: ft_substr prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:52:35 by angrios           #+#    #+#             */
-/*   Updated: 2025/06/16 16:18:51 by angrios          ###   ########.fr       */
+/*   Updated: 2025/06/18 19:21:16 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 #endif
