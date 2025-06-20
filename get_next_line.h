b43abe3161781538e32031f3eccd7cfe6f44985b
Angelo Rios <angelorios@live.com>
2025-06-20 21:26:37 +0200

    add: gnl prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:52:35 by angrios           #+#    #+#             */
-/*   Updated: 2025/06/20 19:49:24 by angrios          ###   ########.fr       */
+/*   Updated: 2025/06/20 20:04:03 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -21,5 +23,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
 
 #endif
