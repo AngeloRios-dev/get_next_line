77fdb98ba20b9467ecc89fc0243010723756b00f
Angelo Rios <angelorios@live.com>
2025-08-06 14:20:02 +0200

    add: test runner script

---
+++ run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Sintax: $0 BUFFER_SIZE file_name"
+	exit 1
+fi
+
+BUFFER_SIZE=$1
+FILE=$2
+
+if [ ! -f "$FILE" ]; then
+	echo "ERROR: file '$FILE' does not exist!"
+	exit 1
+fi
+
+make fclean > /dev/null
+make BUFFER_SIZE=$BUFFER_SIZE > /dev/null
+
+echo "Running test with BUFFER_SIZE=$BUFFER_SIZE and file=$FILE"
+./gnl "$FILE"
